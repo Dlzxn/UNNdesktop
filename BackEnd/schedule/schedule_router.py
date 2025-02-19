e9bdff56d60ae1745b34ebed5f33ce66ed71137f
Dlzxn <aleshamarichev09@gmail.com>
2025-02-19 13:38:51 +0300

    schedule router

--- BackEnd/schedule/schedule_router.py
+++ BackEnd/schedule/schedule_router.py
@@ -1,11 +1,18 @@
 from fastapi import APIRouter, Cookie, Request
 from typing import Optional
-from BackEnd.localserver.Schedule_.schedule import UnnRequest
+from BackEnd.get_info.module_get_info import UnnInfoAboutUser
 
 router_schedule = APIRouter(prefix="/schedule", tags=["schedule"])
 
 
-@router_schedule.get()
-async def schedule_get(request: Request):
-    schedule = UnnRequest()
+@router_schedule.get("{start}:{end}")
+async def schedule_get(request: Request, start: str, end: str):
+    schedule = UnnInfoAboutUser()
+    url = schedule.create_url("s23332", start, end)
+    ruz_on = schedule.get_ruz(url)
+    if ruz_on:
+        return {"ruz": schedule.analyse_ruz(ruz_on)}
+    else:
+        return {"ruz": None}
+
 
