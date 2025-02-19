c86a008cbb2abeaff25115fa4747a216c14167f3
Dlzxn <aleshamarichev09@gmail.com>
2025-02-19 13:42:11 +0300

    schedule Jinja

--- BackEnd/schedule/schedule_router.py
+++ BackEnd/schedule/schedule_router.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter, Cookie, Request
+from fastapi.templating import Jinja2Templates
 from typing import Optional
+
+
 from BackEnd.get_info.module_get_info import UnnInfoAboutUser
+
+
+templates = Jinja2Templates(directory="templates")
+
 
 router_schedule = APIRouter(prefix="/schedule", tags=["schedule"])
 
@@ -10,8 +17,9 @@ async def schedule_get(request: Request, start: str, end: str):
     schedule = UnnInfoAboutUser()
     url = schedule.create_url("s23332", start, end)
     ruz_on = schedule.get_ruz(url)
+    full_ruz= schedule.analyse_ruz(ruz_on)
     if ruz_on:
-        return {"ruz": schedule.analyse_ruz(ruz_on)}
+        return templates.TemplateResponse("schedule.html", context={"ruz": full_ruz})
     else:
         return {"ruz": None}
 
